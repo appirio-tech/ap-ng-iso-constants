6442f6126eb1ce991f712126ee8cdd570e9a0672
Nick Litwin <nlitwin@appirio.com>
2015-11-16 18:41:11 -0800

    Remove unnecessary comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,22 @@
-var gulp = require('gulp');
-var karma = require('karma').server;
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var path = require('path');
-var plumber = require('gulp-plumber');
+var gulp        = require('gulp');
+var karma       = require('karma').server;
+var concat      = require('gulp-concat');
+var uglify      = require('gulp-uglify');
+var rename      = require('gulp-rename');
+var path        = require('path');
+var plumber     = require('gulp-plumber');
 var runSequence = require('run-sequence');
-var jshint = require('gulp-jshint');
+var jshint      = require('gulp-jshint');
 
-/**
- * File patterns
- **/
 
-// Root directory
-var rootDirectory = path.resolve('./');
-
-// Source directory for build process
+var rootDirectory   = path.resolve('./');
 var sourceDirectory = path.join(rootDirectory, './src');
-
 var sourceFiles = [
-
-  // Make sure module files are handled first
   path.join(sourceDirectory, '/**/*.module.js'),
-
-  // Then add all JavaScript files
   path.join(sourceDirectory, '/**/*.js')
 ];
-
 var lintFiles = [
   'gulpfile.js',
-  // Karma configuration
   'karma-*.conf.js'
 ].concat(sourceFiles);
 
@@ -43,25 +30,14 @@ gulp.task('build', function() {
     .pipe(gulp.dest('./dist'));
 });
 
-/**
- * Process
- */
 gulp.task('process-all', function (done) {
   runSequence('jshint', 'test-src', 'build', done);
 });
 
-/**
- * Watch task
- */
 gulp.task('watch', function () {
-
-  // Watch JavaScript files
   gulp.watch(sourceFiles, ['process-all']);
 });
 
-/**
- * Validate source JavaScript
- */
 gulp.task('jshint', function () {
   return gulp.src(lintFiles)
     .pipe(plumber())
@@ -70,9 +46,6 @@ gulp.task('jshint', function () {
     .pipe(jshint.reporter('fail'));
 });
 
-/**
- * Run test once and exit
- */
 gulp.task('test-src', function (done) {
   karma.start({
     configFile: __dirname + '/karma-src.conf.js',
@@ -80,9 +53,6 @@ gulp.task('test-src', function (done) {
   }, done);
 });
 
-/**
- * Run test once and exit
- */
 gulp.task('test-dist-concatenated', function (done) {
   karma.start({
     configFile: __dirname + '/karma-dist-concatenated.conf.js',
@@ -90,9 +60,6 @@ gulp.task('test-dist-concatenated', function (done) {
   }, done);
 });
 
-/**
- * Run test once and exit
- */
 gulp.task('test-dist-minified', function (done) {
   karma.start({
     configFile: __dirname + '/karma-dist-minified.conf.js',
